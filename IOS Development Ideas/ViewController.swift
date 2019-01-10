1d36c74cacd9fdeb6cf081aa48d80f4514936f99
Marc Boanas <Work@marcs-imac>
2019-01-10 17:43:06 +0000

    Initial Commit

---
+++ IOS Development Ideas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS Development Ideas
+//
+//  Created by Work on 10/01/2019.
+//  Copyright © 2019 Work. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IOS Development IdeasTests/IOS_Development_IdeasTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOS_Development_IdeasTests.swift
+//  IOS Development IdeasTests
+//
+//  Created by Work on 10/01/2019.
+//  Copyright © 2019 Work. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_Development_Ideas
+
+class IOS_Development_IdeasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
