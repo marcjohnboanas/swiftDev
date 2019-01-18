a88d4165aaa419df87bf91172672720302c62de3
Work <Work@marcs-imac.home>
2019-01-18 22:54:29 +0000

    completed code for keyboard insets

--- IOS Development Ideas/ViewController.swift
+++ IOS Development Ideas/ViewController.swift
@@ -9,12 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let labelOne: UILabel = {
+        let label = UILabel()
+        label.text = "Scroll Top"
+        label.backgroundColor = .red
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let labelTwo: UILabel = {
+        let label = UILabel()
+        label.text = "Scroll Bottom"
+        label.backgroundColor = .green
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let textField: UITextField = {
+        let tf = UITextField()
+        tf.layer.borderWidth = 2
+        tf.layer.borderColor = UIColor.black.cgColor
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let scrollView: UIScrollView = {
+        let v = UIScrollView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = .cyan
+        return v
+    }()
+    
+    @objc fileprivate func adjustInsetForKeyboard(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let show = notification.name == UIResponder.keyboardWillShowNotification ? true : false
+        let adjustHeight = (keyboardFrame.height + 20) * (show ? 1 : -1)
+        scrollView.contentInset.bottom += adjustHeight
+        scrollView.scrollIndicatorInsets.bottom += adjustHeight
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInsetForKeyboard(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInsetForKeyboard(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // add the scroll view to self.view
+        self.view.addSubview(scrollView)
+        
+        // constrain the scroll view to 8-pts on each side
+        scrollView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8.0).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8.0).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8.0).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8.0).isActive = true
+        
+        scrollView.addSubview(textField)
+        textField.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16).isActive = true
+        textField.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: -40).isActive = true
+        textField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 1200).isActive = true
+        textField.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16).isActive = true
+        textField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        textField.delegate = self
     }
-
-
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
